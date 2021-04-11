4d252f359edd27aed60e1aec58a18216fefbbe56
Areeba Aziz <aziza11@mcmaster.ca>
2021-04-10 21:45:51 -0400

    added links to header buttons

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,28 +1,29 @@
-import { 
-    Navbar, 
-    Nav, 
-    NavDropdown,
-    Form, 
-    FormControl, 
-    Button
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+  Link
 } from 'react-bootstrap';
 import { Search, PersonCircle } from 'react-bootstrap-icons';
 
 export default function Header() {
-    return (<>
-        <Navbar bg="primary" variant="dark">
-            <Navbar.Brand href="/" as='h1' style={{marginRight:'2em'}}>PodSquad</Navbar.Brand>
-            <Form inline style={{width:'100%'}}>
-                <FormControl type="text" placeholder="Search" 
-                    style={{width:'20em', height: '2em'}} className="mr-lg-2" />
-                <Button variant="primary"><Search /></Button>
-            </Form>
-            <Nav className="justify-content-end" style={{width: '100%'}}>
-                <Nav.Link href="#home">Recommendations</Nav.Link>
-                <Nav.Link href="#features">Rankings</Nav.Link>
-                <Nav.Link href="#pricing">Profile 
-                    <PersonCircle style={{ marginLeft: '1em' }} size={30}/></Nav.Link>
-            </Nav>
-        </Navbar>
-    </>);
+  return (<>
+    <Navbar bg="primary" variant="dark">
+      <Navbar.Brand href="/" style={{ marginRight: '2em', fontSize: '1.6em' }}>PodSquad</Navbar.Brand>
+      <Form inline style={{ width: '100%' }}>
+        <FormControl type="text" placeholder="Search"
+          style={{ width: '20em', height: '1.9em' }} className="mr-lg-2" />
+          <a href="/search" className="btn btn-primary"><Search /></a>
+      </Form>
+      <Nav className="justify-content-end" style={{ width: '100%' }}>
+        <Nav.Link href="/recommendations">Recommendations</Nav.Link>
+        <Nav.Link href="/rankings">Rankings</Nav.Link>
+        <Nav.Link href="/profile">Profile
+                    <PersonCircle style={{ marginLeft: '1em' }} size={30} /></Nav.Link>
+      </Nav>
+    </Navbar>
+  </>);
 }
