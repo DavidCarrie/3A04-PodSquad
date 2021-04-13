8c2577b61d1fa6fa9da245608c8bac55f615afc0
Brandon <duongb9@mcmaster.ca>
2021-04-13 19:25:02 -0400

    Rankings now require spotify log in!

--- frontend/src/views/RankingsView/index.js
+++ frontend/src/views/RankingsView/index.js
@@ -1,3 +1,99 @@
-export default function RankingsView() {
-    return <p>This is the Rankings view!</p>
+import React from "react";
+import ProfileView from "../ProfileView";
+import {ajax} from "rxjs/ajax";
+
+export const authEndpoint = 'https://accounts.spotify.com/authorize?';
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "17d5bf25cae142aaa9512ecff18c1bca";
+const redirectUri = "http://localhost:3000/rankings";
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
+// Get the hash of the url
+const hash = window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce(function(initial, item) {
+        if (item) {
+            var parts = item.split("=");
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+        }
+        return initial;
+    }, {});
+window.location.hash = "";
+
+class RankingsView extends React.Component{
+
+    constructor() {
+        super();
+        this.state = {
+            token: null,
+            item: {
+                album: {
+                    images: [{ url: "" }]
+                },
+                name: "",
+                artists: [{ name: "" }],
+                duration_ms:0,
+            },
+            is_playing: "Paused",
+            progress_ms: 0
+        };
+
+        this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+    }
+    getCurrentlyPlaying(token) {
+        // Make a call using the token
+        ajax({
+            url: "https://api.spotify.com/v1/me/player",
+            type: "GET",
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader("Authorization", "Bearer " + token);
+            },
+            success: (data) => {
+                this.setState({
+                    item: data.item,
+                    is_playing: data.is_playing,
+                    progress_ms: data.progress_ms,
+                });
+            }
+        });
+    }
+
+    componentDidMount() {
+        // Set token
+        let _token = hash.access_token;
+        if (_token) {
+            // Set token
+            this.setState({
+                token: _token
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className='card w-75' style={{ left: '12.5%'}}>
+                <p>This is the Rankings view!</p>
+                <header className="App-header">
+                    {!this.state.token && (
+                        <a
+                            className="btn btn--loginApp-link"
+                            href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+                        >
+                            Login to Spotify
+                        </a>
+                    )}
+                    {this.state.token && (
+                        // Spotify stuff
+                        <a>Logged in!</a>
+                    )}
+
+                </header>
+            </div>
+        )
+    }
 }
+
+export default RankingsView;
